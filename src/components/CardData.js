d2dc30b25fa0c84ab72ee89b3be6f80e068cdcf9
Lance Martija <jacobmartija03@gmail.com>
2021-04-04 14:15:40 +0800

    removed some text shadows

--- src/components/CardData.js
+++ src/components/CardData.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const CardData = ({ region, cases, recovered, deaths }) => {
 	return (
-		<div className='box tc bg-light-blue dib m-2 black text-shadow'>
-			<h1 className='title title-font f3'>{region}</h1>
+		<div className='box tc bg-light-blue dib m-2 black'>
+			<h1 className='title title-font f3 text-shadow'>{region}</h1>
 			<h1 className='title f4'>Cases: {cases}</h1>
 			<h1 className='title f4'>Recovered: {recovered}</h1>
 			<h1 className='title f4'>Deaths: {deaths}</h1>
